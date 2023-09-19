42c3c018abaa5b3b85a76176b825b1cc8051559c
felipe <fg631173@gmail.com>
2023-09-19 17:27:45 -0300

    o inferno 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("aa",(req,res) => {
-    res.send("o caralho")
+    res.send({aaaaaaaaa:"vai se fera"})
 })
 
 app.listen(process.env.PORT || 3000,() => console.log("ta funcionado"))
