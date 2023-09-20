73d83b99a15489d6384163fcd80fff6b0245ca6d
felipe <fg631173@gmail.com>
2023-09-20 16:53:50 -0300

    v9

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const app = express()
 
 app.get("/aa",(req,res) => {
-    res.send({aaaaaaaaa:"vai se fera"})
+    res.send({aaaaaaaaa:"seguinte tiago isso aqui é uma api hospedada definitivamente e ela é capaz de suprir todas as nossas necessidades amanha eu te conto com detalhes como isso funciona simplesmente eu to chorando eu finalmente aprendi oq eu precisava pra fazer isso acontecer"})
 })
 
 app.listen(process.env.PORT || 3000,() => console.log("ta funcionado"))
